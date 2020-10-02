36fbfc057cca521ff3153ba66c3e1aac51648578
Vladislav578888 <limon.tort@mail.ru>
2020-10-02 22:27:33 +0300

    feat: solve index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+  if (!array)
   return 0;
+  return array.reduce((acc, i) => {
+    if (i < acc)
+    acc = i;
+    return acc;
+  }, 0); 
 }
 
 exports.max = function max (array) {
+  if (!array)
   return 0;
+  return array.reduce((acc, i) => {
+    if (i > acc)
+     acc = i;
+    return acc;
+  }, 0); 
 }
 
 exports.avg = function avg (array) {
+  if (!array || array.length === 0)
   return 0;
+  return array.reduce((acc, i) => acc + i, 0) / array.length; 
 }
